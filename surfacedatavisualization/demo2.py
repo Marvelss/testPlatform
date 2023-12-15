49c05fcc3f6eabb74b44d0aabf9a6c1a57198eb8
SakuraFox <982001940@qq.com>
2023-12-15 10:56:26 +0800

    自定义有效区域,并根据最大列间距,将行数自动扩展,补全为方阵

--- surfacedatavisualization/demo2.py
+++ surfacedatavisualization/demo2.py
@@ -1,4 +1,4 @@
-# 获取最大有效区域
+# 自定义有效区域,并自动补全为方阵
 import math
 
 import pandas as pd
@@ -7,53 +7,28 @@ import seaborn as sns
 from tqdm import tqdm
 from PIL import Image
 
-min_row, max_row, min_col, max_col = 0, 0, 0, 0
-data = pd.read_excel('2266s.xlsx', header=None)
-# 定义一个空数组，用于存储出现非1数值的行数
-rows_with_non_one_values = []
+# 自定义区域范围
+min_row, max_row, min_col, max_col = 38, 60, 71, 100
 
-# 遍历每一行内容
-for index, row in data.iterrows():
-    # 检查每个单元格的数值是否为1
-    if any(row != 1):
-        # 如果有非1的数值，则将该行数添加到数组中
-        rows_with_non_one_values.append(index)
+use_col_list = []
+sideLength = max_col - min_col - max_row + min_row
+use_min_row, use_max_row = min_row - sideLength / 2, max_row + sideLength / 2
+print('间距上下扩展数值')
+print(sideLength)
+print('------------------------------------')
+print('扩展后最大最小行')
+print(use_min_row, use_max_row)
+for i in range(min_col, max_col + 1):
+    use_col_list.append(i)
+print('------------------------------------')
+print('y轴列')
+print(use_col_list)
 
-# 打印包含非1数值的行数数组
-print("包含非1数值的行数数组:")
-print(rows_with_non_one_values)
-print(rows_with_non_one_values[0] + 1, rows_with_non_one_values[-1] + 1)
-
-# 定义一个空数组，用于存储出现非1数值的列数
-columns_with_non_one_values = []
-
-# 遍历每一列内容
-for column in data.columns:
-    # 检查每个单元格的数值是否为1
-    if any(data[column] != 1):
-        # print(data[column].head(59))
-        # 如果有非1的数值，则将该列数添加到数组中
-        columns_with_non_one_values.append(column)
-
-# 打印包含非1数值的列数数组
-print("包含非1数值的列数数组:")
-print(columns_with_non_one_values[0], columns_with_non_one_values[-1])
-# 最大最小行列
-min_row = rows_with_non_one_values[0] + 1
-max_row = rows_with_non_one_values[-1] + 1
-min_col = columns_with_non_one_values[0]
-max_col = columns_with_non_one_values[-1]
-
-# 实际热力图的行列
-print(min_row, max_row)
-print(columns_with_non_one_values)
-fillLength = max(max_row - min_row, max_col - min_col)
-print(fillLength)
 df1 = pd.read_excel('2266s.xlsx',
                     header=None,
-                    skiprows=min_row,
-                    nrows=fillLength,
-                    usecols=columns_with_non_one_values)
+                    skiprows=int(use_min_row),
+                    nrows=int(use_max_row - use_min_row),
+                    usecols=use_col_list)
 # df.to_excel('a1.xlsx')
 # plt.xticks(5, 31)
 f, ax = plt.subplots(1, 1)
@@ -66,8 +41,8 @@ ax.set_title('Group: Day Of Years:')
 # 获取x轴标签
 # x_labels = [label.get_text() for label in ax.get_xticklabels()]
 
-print('----')
-print(math.ceil(fillLength/len(ax.get_xticklabels())))
+print('------------------------------------')
+print('x轴刻度')
 y_label = []
 num = min_row
 for _ in range(len(ax.get_xticklabels())):
@@ -77,4 +52,4 @@ print(y_label)
 # 打印x轴标签
 # print()
 ax.set_yticklabels(y_label)
-f.savefig(('1.png'), dpi=500, bbox_inches='tight')
+f.savefig(('14.png'), dpi=500, bbox_inches='tight')
